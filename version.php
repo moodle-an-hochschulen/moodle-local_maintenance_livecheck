502e0bc53ca03d9b849bcf6f17be199327563677
Kathrin Osswald <kathrin.osswald@uni-ulm.de>
2019-01-21 09:04:10 +0100

    Release v3.6-r1

--- version.php
+++ version.php
@@ -26,7 +26,7 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'local_maintenance_livecheck';
-$plugin->version = 2018052900;
-$plugin->release = 'v3.5-r1';
+$plugin->version = 2019012100;
+$plugin->release = 'v3.6-r1';
 $plugin->requires = 2018120300;
 $plugin->maturity = MATURITY_STABLE;
