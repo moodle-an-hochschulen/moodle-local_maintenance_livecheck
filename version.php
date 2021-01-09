c4e404e9f86c98290f9f496c6b53fb1df1d16d6e
Alexander Bias <alexander.bias@uni-ulm.de>
2021-01-09 21:11:14 +0100

    Improvement: Declare which major stable version of Moodle this plugin supports

--- version.php
+++ version.php
@@ -29,4 +29,5 @@ $plugin->component = 'local_maintenance_livecheck';
 $plugin->version = 2020071600;
 $plugin->release = 'v3.9-r1';
 $plugin->requires = 2020061500;
+$plugin->supported = [39, 39];
 $plugin->maturity = MATURITY_STABLE;
