968e82bbf5f2f8a05e97d5f6736890bdf1e2b76f
Alexander Bias <bias@alexanderbias.de>
2022-06-28 21:21:11 +0200

    Make codechecker happy again

--- lib.php
+++ lib.php
@@ -23,8 +23,6 @@
  * @license   http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
 
-defined('MOODLE_INTERNAL') || die();
-
 /**
  * Insert the necessary JS code to each page by abusing Moodle's *_extend_navigation() hook.
  *
